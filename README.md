0c6a46ad53477c4896a8dd7dfa49a5984478f242
Ian Johnson <tacoda@hey.com>
2025-06-27 14:06:47 +0100

    Add page and result models

--- README.md
+++ README.md
@@ -26,3 +26,19 @@ Scraper.text("https://www.adafruit.com/product/5803")
 document = _
 document.at_css("[itemprop='aviailability']").text
 ```
+
+
+## Add Page and Result Models
+
+```sh
+r g scaffold Page url check_type selector match_text
+r g model Result page:belongs_to success:boolean
+r db:migrate
+r s
+```
+
+- Update root route
+- Update form to use a select for the check_type
+- Add validation to Page model
+- Add run_check! to Page model
+- Add Result association to Page model
---
+++ app/models/page.rb
@@ -0,0 +1,27 @@
+class Page < ApplicationRecord
+  has_many :results
+
+  validates :url, presence: true
+  validates :check_type, presence: true
+  validates :selector, presence: true
+  validates :match_text, presence: { if: :check_text? }
+
+  def run_check!
+    scraper = Scraper.new(url)
+    result = case check_type
+    when "text"
+               scraper.text(selector: selector).downcase == match_text.downcase
+    when "exists"
+               scraper.present?(selector: selector)
+    when "not_exists"
+               !scraper.present?(selector: selector)
+    end
+    results.create(success: result)
+  end
+
+  private
+
+  def check_text?
+    check_type == "text"
+  end
+end
